f91de0616e78fc0e5cb9029a4e61933f12b87978
DSGilfillan <daniel.gilfillan@ukipme.com>
2020-02-05 11:38:47 +0000

    Submission form and auto-mated tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # laravel-tutorial
-A Laravel Tutorial App for first timers 
+A Laravel Tutorial App for first timers
+
+Reference material: https://laravel-news.com/your-first-laravel-application
--- links/routes/web.php
+++ links/routes/web.php
@@ -16,3 +16,22 @@ Route::get('/', function () {
 
     return view('welcome', ['links' => $links]);
 });
+Route::get('/submit', function () {
+    return view('submit', ['links' => $links]);
+});
+
+// Post the Data
+
+use Illuminate\Http\Request;
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
